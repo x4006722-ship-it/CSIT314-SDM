18de9b65819e0081e58c76bed3dcab67d7d4a3c2
Shuai Cheng <zino1625441786@gmail.com>
2026-04-07 19:46:55 +0800

    website

--- src/main/java/com/uow/config/WebConfig.java
+++ src/main/java/com/uow/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/login.html");
+        registry.addViewController("/").setViewName("forward:/LoginPage.html");
     }
 }
